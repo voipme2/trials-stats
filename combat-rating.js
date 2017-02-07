60f775b1a7b8d73fc8a6c04029940b65e875ad37
Pat Laughlin <voipme2@users.noreply.github.com>
2017-02-07 14:01:04 -0500

    adding in combat rating shell

---
+++ combat-rating.js
@@ -0,0 +1,23 @@
+// lookup the player (and all characters)
+function lookup(playerName, cb) {
+
+}
+
+// get all matches for all types (minus trials)
+//  - group them by "mode"
+function getMatches(playerObj, cb) {
+
+}
+
+// for each match in mode
+//  - get my combat rating
+//  - average combat rating of everyone else
+//  - if team mode
+//    - get average of other team members
+//    - get average of enemy team members
+function getData(match, cb) {
+
+}
+
+//  - Response.data.entries[].extended.combatRating.basic.value
+//  - Response.data.entries[].player.destiyUserInfo.displayName
